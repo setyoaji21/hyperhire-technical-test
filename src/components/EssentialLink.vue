a999cb45cb65843991bf85b6bec39f4b40730aa6
Dendie <lokassd@gmail.com>
2024-04-25 16:18:49 +0700

    html to quasar

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-item
+  <!-- <q-item
     clickable
     tag="a"
     target="_blank"
@@ -16,33 +16,219 @@
       <q-item-label>{{ props.title }}</q-item-label>
       <q-item-label caption>{{ props.caption }}</q-item-label>
     </q-item-section>
-  </q-item>
+  </q-item> -->
+  <div class="frame-2">
+    <div class="div-wrapper">
+      <div class="text-wrapper-4">풀타임, 파트타임</div>
+    </div>
+    <div class="polygon-wrapper">
+      <img class="polygon" src="img/polygon-1.svg" />
+    </div>
+  </div>
+  <div class="text-wrapper-5">개발자가 필요하신가요?</div>
+  <div class="frame-wrapper">
+    <div class="frame-3">
+      <div class="frame-4">
+        <div class="image-wrapper">
+          <img class="image" src="img/image-17.png" />
+        </div>
+        <div class="frame-5">
+          <div class="text-wrapper-6">Abhishek Gupta</div>
+          <div class="text-wrapper-7">마케팅 · 2y+</div>
+        </div>
+      </div>
+      <div class="frame-6">
+        <div class="frame-7">
+          <div class="frame-8">
+            <div class="text-wrapper-8">마케팅 콘텐츠 제작</div>
+          </div>
+          <div class="frame-8">
+            <div class="text-wrapper-8">인스타그램 관리</div>
+          </div>
+        </div>
+        <div class="group-2">
+          <div class="frame-9">
+            <div class="text-wrapper-8">트위터 관리</div>
+          </div>
+          <div class="frame-10">
+            <div class="text-wrapper-8">블로그 글 작성</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="group-3">
+    <div class="frame-3">
+      <div class="frame-4">
+        <div class="img-wrapper">
+          <img class="image" src="img/image-17-3.png" />
+        </div>
+        <div class="frame-5">
+          <div class="text-wrapper-6">Abhishek Gupta</div>
+          <div class="text-wrapper-7">마케팅 · 2y+</div>
+        </div>
+      </div>
+      <div class="frame-6">
+        <div class="frame-7">
+          <div class="frame-8">
+            <div class="text-wrapper-8">마케팅 콘텐츠 제작</div>
+          </div>
+          <div class="frame-8">
+            <div class="text-wrapper-8">인스타그램 관리</div>
+          </div>
+        </div>
+        <div class="group-2">
+          <div class="frame-9">
+            <div class="text-wrapper-8">트위터 관리</div>
+          </div>
+          <div class="frame-10">
+            <div class="text-wrapper-8">블로그 글 작성</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="group-4">
+    <div class="frame-11">
+      <div class="frame-12">
+        <div class="group-5">
+          <img class="img" src="img/image-17-2.png" />
+        </div>
+        <div class="frame-5">
+          <div class="text-wrapper-9">Abhishek Gupta</div>
+          <div class="text-wrapper-10">마케팅 · 2y+</div>
+        </div>
+      </div>
+      <div class="frame-13">
+        <div class="frame-13">
+          <div class="frame-14">
+            <div class="text-wrapper-11">마케팅 콘텐츠 제작</div>
+          </div>
+          <div class="frame-14">
+            <div class="text-wrapper-11">인스타그램 관리</div>
+          </div>
+        </div>
+        <div class="group-6">
+          <div class="frame-15">
+            <div class="text-wrapper-11">트위터 관리</div>
+          </div>
+          <div class="frame-16">
+            <div class="text-wrapper-11">블로그 글 작성</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <img class="navi-caret-right" src="img/caret-right.svg" />
+  <img class="navi-caret-right-2" src="img/caret-right-2.svg" />
+  <div class="overlap-wrapper">
+    <div class="overlap-group">
+      <div class="frame-17">
+        <div class="group-wrapper">
+          <img class="group-7" src="img/group-5095.png" />
+        </div>
+        <div class="text-wrapper-12">월 100만원</div>
+      </div>
+      <img class="polygon-2" src="img/polygon-1-2.svg" />
+    </div>
+  </div>
+  <div class="frame-18">
+    <div class="frame-19">
+      <img class="frame-20" src="img/frame-5065-2.svg" />
+      <div class="frame-21">
+        <div class="text-wrapper-13">해외 마케팅</div>
+      </div>
+    </div>
+    <div class="frame-19">
+      <img class="frame-20" src="img/frame-5031-2.svg" />
+      <div class="frame-21">
+        <div class="text-wrapper-13">퍼블리셔</div>
+      </div>
+    </div>
+    <div class="frame-19">
+      <img class="frame-20" src="img/frame-5031.svg" />
+      <div class="frame-21">
+        <div class="text-wrapper-13">캐드원(제도사)</div>
+      </div>
+    </div>
+    <div class="frame-19">
+      <img class="frame-20" src="img/frame-5065-4.svg" />
+      <div class="frame-21">
+        <div class="text-wrapper-13">해외 세일즈</div>
+      </div>
+    </div>
+    <div class="frame-19">
+      <img class="frame-20" src="img/frame-5065-3.svg" />
+      <div class="frame-21">
+        <div class="text-wrapper-14">해외 CS</div>
+      </div>
+    </div>
+    <div class="frame-19">
+      <img class="frame-22" src="img/frame-5065.svg" />
+      <div class="frame-21">
+        <div class="text-wrapper-13">해외 마케팅</div>
+      </div>
+    </div>
+  </div>
+  <div class="frame-23">
+    <div class="frame-24">
+      <img class="line" src="img/line-13.svg" />
+      <div class="text-wrapper-15">평균 월 120만원</div>
+      <p class="text-wrapper-16">임금을 해당 국가를 기준으로 계산합니다.</p>
+    </div>
+    <div class="frame-24">
+      <img class="line" src="img/line-14.svg" />
+      <div class="text-wrapper-15">최대 3회 인력교체</div>
+      <p class="text-wrapper-16">
+        막상 채용해보니 맞지 않아도 걱정하지 마세요.
+      </p>
+    </div>
+    <div class="frame-24">
+      <img class="line" src="img/line-14-2.svg" />
+      <div class="text-wrapper-15">평균 3일, 최대 10일</div>
+      <p class="text-wrapper-16">
+        급하게 사람이 필요한 경우에도 빠른 채용이 가능합니다.
+      </p>
+    </div>
+  </div>
+  <div class="text-wrapper-17">FadeInUp(500ms)</div>
+  <div class="text-wrapper-18">FadeInUp(duration 500ms)</div>
+  <p class="text-wrapper-19">
+    FadeIn tooltips, bottom card, and arrows with the title (500ms)
+  </p>
+  <p class="text-wrapper-20">fade in 3 at once(500ms)</p>
+  <p class="fadein-slides">
+    fadeIn(500ms)<br /><br />Slides to the left by one every 5 seconds
+  </p>
+  <p class="fade-in-after-title">
+    FadeIn after title and subtitle shown(delay 300ms)
+  </p>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'EssentialLink'
-})
+  name: "EssentialLink",
+});
 
 const props = defineProps({
   title: {
     type: String,
-    required: true
+    required: true,
   },
 
   caption: {
     type: String,
-    default: ''
+    default: "",
   },
 
   link: {
     type: String,
-    default: '#'
+    default: "#",
   },
 
   icon: {
     type: String,
-    default: ''
-  }
-})
+    default: "",
+  },
+});
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="lHh Lpr lFf" class="desktop">
     <q-header elevated>
       <q-toolbar>
         <q-btn
@@ -11,25 +11,15 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-toolbar-title> Quasar App </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+        <q-item-label header> Essential Links </q-item-label>
 
         <EssentialLink
           v-for="link in linksList"
@@ -42,65 +32,141 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer class="overlap-group-wrapper">
+      <div class="overlap-group-2">
+        <div class="rectangle"></div>
+        <img class="element-2" src="img/1.svg" />
+        <p class="text-wrapper-21">
+          우리는 국가의 장벽을 넘어 최고의 인재를 매칭해드립니다.
+        </p>
+        <div class="text-wrapper-22">010-0000-0000</div>
+        <div class="text-wrapper-23">aaaaa@naver.com</div>
+        <p class="text-wrapper-24">서울특별시 강남대로 479, 지하 1층 238호</p>
+        <div class="text-wrapper-25">427-86-01187</div>
+        <div class="text-wrapper-26">하이퍼하이어</div>
+        <div class="text-wrapper-27">김주현</div>
+        <div class="text-wrapper-28">사업자등록번호 CIN</div>
+        <div class="text-wrapper-29">대표 CEO</div>
+        <div class="text-wrapper-30">상호명</div>
+        <div class="text-wrapper-31">U74110DL2016PTC290812</div>
+        <div class="text-wrapper-32">주소 ADDRESS</div>
+        <p class="d-street-number">
+          D-138, Street number 11, Jagjeet Nagar, North East Delhi, New Delhi,
+          <br />110053 India
+        </p>
+        <div class="text-wrapper-33">Hyperhire India Private Limited</div>
+        <div class="text-wrapper-34">Juhyun Kim</div>
+        <div class="text-wrapper-35">ⓒ 2023 Hyperhire</div>
+        <div class="frame-25">
+          <div class="frame-26">
+            <img class="frame-20" src="img/frame-5042.svg" />
+            <div class="frame-27">
+              <div class="text-wrapper-36">해외 개발자 원격 채용</div>
+            </div>
+          </div>
+          <div class="frame-28">
+            <div class="text-wrapper-37">바로가기</div>
+            <div class="iconly-light-outline"></div>
+          </div>
+        </div>
+        <div class="frame-29">
+          <div class="frame-30">
+            <img class="frame-20" src="img/frame-5042-3.svg" />
+            <div class="frame-27">
+              <div class="text-wrapper-36">외국인 원격 채용 (비개발)</div>
+            </div>
+          </div>
+          <div class="frame-28">
+            <div class="text-wrapper-37">바로가기</div>
+            <div class="iconly-light-outline-2"></div>
+          </div>
+        </div>
+        <div class="frame-31">
+          <div class="frame-26">
+            <img class="frame-20" src="img/frame-5042-2.svg" />
+            <div class="frame-27">
+              <div class="text-wrapper-36">한국어 가능 외국인 채용</div>
+            </div>
+          </div>
+          <div class="frame-28">
+            <div class="text-wrapper-37">바로가기</div>
+            <div class="iconly-light-outline-3"></div>
+          </div>
+        </div>
+        <div class="frame-32">
+          <div class="frame-26">
+            <img class="frame-20" src="img/frame-5043.svg" />
+            <div class="frame-27">
+              <div class="text-wrapper-36">해외 개발자 활용 서비스</div>
+            </div>
+          </div>
+          <div class="frame-28">
+            <div class="text-wrapper-37">바로가기</div>
+            <div class="iconly-light-outline-4"></div>
+          </div>
+        </div>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
 
 defineOptions({
-  name: 'MainLayout'
-})
+  name: "MainLayout",
+});
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
 
-const leftDrawerOpen = ref(false)
+const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
